3bcb1f3405c922095dd6018d68e7fc327dcb735f
benyoo5222 <benyoo5222@gmail.com>
2018-04-16 17:48:43 +0000

    edited both files. added to the header

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,25 @@
 var request = require('request');
+var token = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
 
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+
+  // creating a variable of object thats called options to show the url and the header
+  var options={
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+
+  // adding headers now to satisfy the request
+  headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${token.GITHUB_TOKEN}`
+    }
+  };
+
+// instead of url we are passing the whole options object which includes the url and the headers
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
